0a4f85e96c6d4a6376a4c26a18623f3931b30922
Brandon <p4r4d0x42@gmail.com>
2014-04-18 20:00:40 -0700

    Got the correct version of npgsql added as a reference in visual studios. Need to have a copy of npgsq..dll inside of the Asset folder in order to get unity to work with it correctly. Was also able to establish a connection with schemaverse and run a query for my player data. Great place to build from now that that is working as expected.

--- Assets/Scripts/ConnectToDB.cs
+++ Assets/Scripts/ConnectToDB.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using Npgsql;
+using UnityEngine;
 using System.Collections;
-// For reference http://www.codeproject.com/Articles/30989/Using-PostgreSQL-in-your-C-NET-application-An-intr
+
+
+
+// For reference 
+// http://www.codeproject.com/Articles/30989/Using-PostgreSQL-in-your-C-NET-application-An-intr
+// http://forum.unity3d.com/threads/9051-Test-Connect-Script-to-PostgreSQL-Server
+// http://forum.unity3d.com/threads/7373-using-Npgsql-dll-to-access-PostgreSQL-server
+// http://forum.unity3d.com/threads/139887-Data-does-not-exist-in-the-namespace-System
 
 public class ConnectToDB : MonoBehaviour
 {
@@ -8,18 +17,16 @@ public class ConnectToDB : MonoBehaviour
     private int Port = 5432; // server port number
 
     private string
-        ServerName = "The Schemaverse",
-        ServerIP = "db.schemaverse.com",
-        Username = "mrfreeman";
+        DBname = "schemaverse",
+        Host = "db.schemaverse.com",
+        User = "mrfreeman";
 
     public bool StorePassword;
 
     private string Password = "M*EdCXpDm*9mQJ9FJ";
     
         
-
-
-
+ 
 
     void OnGUI()
     {
@@ -29,10 +36,62 @@ public class ConnectToDB : MonoBehaviour
         
         // Connection Settings
         // Pixels from left side of screen, Pixels from top left of screen, button width, button height
-        GUI.Label(new Rect(35, 75, 256, 30), string.Format("Server Address: {0}", ServerIP));
+        GUI.Label(new Rect(35, 75, 256, 30), string.Format("Server Address: {0}", Host));
         GUI.Label(new Rect(35, 95, 256, 30), string.Format("Server Port: {0}", Port));
-        GUI.Label(new Rect(35, 115, 256, 30), string.Format("Connecting As: {0}", Username));
-        if (GUI.Button(new Rect(64, 140, 128, 30), "Connect")) { Debug.Log("Connecting....."); }
+        GUI.Label(new Rect(35, 115, 256, 30), string.Format("Connecting As: {0}", User));
+        if (GUI.Button(new Rect(64, 140, 128, 30), "Connect"))
+        {
+            Debug.Log("Connecting.....");
+
+            try
+            {
+                var dbcon = new NpgsqlConnection(string.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",Host,Port,User,Password,DBname));
+
+                dbcon.Open();
+
+                Debug.Log("Connection should be open");
+                
+                // SELECT username, balance, fuel_reserve FROM my_player;
+                var dbcmd = dbcon.CreateCommand(); // Used for issuing commands?
+                dbcmd.CommandText = "SELECT username, balance, fuel_reserve FROM my_player"; // Test connection by running this query
+
+                var reader = dbcmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    string username = reader["username"].ToString();
+
+                    string balance = reader["balance"].ToString();
+
+                    string fuel_reserve = reader["fuel_reserve"].ToString();
+
+                    Debug.Log(string.Format("Username: {0}, Balance: {1}, Fuel Reserve: {2}",username,balance,fuel_reserve));
+
+                }
+
+                // clean up
+
+                reader.Close();
+                reader = null;
+
+                dbcmd.Dispose();
+                dbcmd = null;
+
+                dbcon.Close();                
+                dbcon = null;
+                Debug.Log("Connection should be closed");
+   
+
+            }
+            catch (Exception msg)
+            {
+                // something went wrong, and you wanna know why
+                //lMessageBox.Show(msg.ToString());l
+                Debug.LogError(msg.ToString());
+                throw;
+            }
+        }
         
 
     }
